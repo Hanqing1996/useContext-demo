4e52fea9853d296322af43d39b553238bab42b31
zhq <zhq199622@163.com>
2020-02-18 16:37:25 +0800

    useContent 往往搭配 useReducer/userState 一起使用

---
+++ demo1/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, createContext,useContext} from 'react'
+
+const C = createContext(null);
+
+function App() {
+
+  console.log('App is rendered')
+  const [n, setN] = useState(0);// 用 useReducer 也可以
+  return (
+    <C.Provider value={{ n, setN }}>
+      <div>
+        <Baba />
+      </div>
+    </C.Provider>
+  );
+}
+
+function Baba() {
+  const { n, setN } = useContext(C);
+  return (
+    <div>
+      我是爸爸 n: {n} <Child />
+    </div>
+  );
+}
+
+function Child() {
+  const { n, setN } = useContext(C);
+  const onClick = () => {
+    setN(i => i + 1);
+  };
+  return (
+    <div>
+      我是儿子 我得到的 n: {n}
+      <button onClick={onClick}>+1</button>
+    </div>
+  );
+}
+
+export default App
